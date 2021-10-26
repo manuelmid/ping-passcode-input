368332dcfd98a3fb9e3b4a9df6175456513c62da
Manuel Duran <manuelmid99@gmail.com>
2021-10-26 20:42:52 +0200

    input passcode crated, all styles meded and finiched. Working on the data capture and another functions

---
+++ scripts.js
@@ -0,0 +1,19 @@
+const inputs = document.querySelectorAll("div#countainer input");
+var container = document.getElementById("container");
+
+container.onkeyup = function(e) {
+    var target = e.srcElement;
+    var maxLength = parseInt(target.attributes["maxlength"].value, 10);
+    var myLength = target.value.length;
+    if (myLength >= maxLength) {
+        var next = target;
+        while (next = next.nextElementSibling) {
+            if (next == null)
+                break;
+            if (next.tagName.toLowerCase() == "input") {
+                next.focus();
+                break;
+            }
+        }
+    }
+}
